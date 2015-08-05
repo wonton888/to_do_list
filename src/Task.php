83c3e79c4c8b5364c91ce955c151e2a40c0ac719
Alex Costa & Adam Won <student@epicodus.com>
2015-08-05 10:37:15 -0700

    Made static function in TAsk CLASS

--- src/Task.php
+++ src/Task.php
@@ -31,6 +31,10 @@
             array_push($_SESSION['list_of_tasks'], $this);
 
           }
+          static function getAll()
+          {
+              return $_SESSION['list_of_tasks'];
+          }
 
 
 
