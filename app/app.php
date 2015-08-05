8bcc60245130f5c5f28a3dc8fa43044423752b1e
Alex Costa & Adam Won <student@epicodus.com>
2015-08-05 10:24:06 -0700

    Made our class instances

--- app/app.php
+++ app/app.php
@@ -4,5 +4,27 @@
 
     $app = new Silex\Application();
 
+    $app->get("/", function(){
+
+      $test_task = new Task("Go to Bathroom.");
+      $test_task2 = new Task("Go through Dental Hygeiene process");
+      $test_task3 = new Task("Take a Bath/Shower");
+      $test_task4 = new Task("Put on Approriate Attire");
+      $fifth_task = new Task("Go to Epicodus class on time!");
+
+      $list_of_tasks = array($test_task, $test_task2, $test_task3,$test_task4,$fifth_task);
+      $output = "";
+
+      foreach ($list_of_tasks as $task){
+            $output = $output . "<p>" . $task->getDescription() . "</p>";
+
+      }
+
+      return $output;
+
+    });
+
+
+
     return $app;
  ?>
